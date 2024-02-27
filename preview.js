3989a5e1af9f3e56b6a86dfcc231688ec428baff
Kat-Erina <nalchevanidzekaterina@gmail.com>
2024-02-27 16:57:43 +0400

    started working on preview section, styling and functionality,

---
+++ preview.js
@@ -0,0 +1,88 @@
+let person = JSON.parse(localStorage.getItem("person")) || "";
+
+let education = JSON.parse(localStorage.getItem("education")) || "";
+let experience = JSON.parse(localStorage.getItem("experience-details")) || "";
+let skills = JSON.parse(localStorage.getItem("skills-container")) || "";
+let references = JSON.parse(localStorage.getItem("reference")) || "";
+let image = JSON.parse(localStorage.getItem("image")) || "";
+
+console.log(references);
+
+if (image != "") {
+  document.querySelector("img").src = image;
+  document.querySelector("img").classList.remove("hidden");
+}
+
+document.querySelector(".applying-for").textContent = person["desired-job"];
+document.querySelector(".address").textContent = person.address;
+document.querySelector(
+  ".contact"
+).textContent = `Email: ${person.email}, Mobile: ${person.phone}`;
+document.querySelector(".about-me").textContent = person["about-me"];
+
+education.forEach((element) => {
+  document.querySelector(".education-main-division").insertAdjacentHTML(
+    "beforeBegin",
+    `  <div class="education-division">
+  <h4 class="university-name">${element.university}</h4>
+  <p class="faculty">${element.specialty}</p>
+  <p class="dates">${element["start-date"]}/${element["finish-date"]}</p>
+</div>
+<hr class="hr"/>`
+  );
+});
+
+experience.forEach((element) => {
+  document.querySelector(".experience-main-division").insertAdjacentHTML(
+    "beforeBegin",
+    `  <div class="experience-division">
+    <h4 class="employer-name">${element["employer-name"]}</h4>
+    <p class="occupation">${element.occupation}</p>
+    <p class="job-description">${element["job-description"]}</p>
+    <p class="dates">${element["job-start-date"]}/${element["job-finish-date"]}</p>
+  </div>
+  <hr class="hr"/>`
+  );
+});
+
+skills.forEach((element) => {
+  let level = "";
+  if (element["level-label"] == "beginner") {
+    level = '<i class="fa-solid fa-star"></i>';
+  }
+  if (element["level-label"] == "intermediate") {
+    level = '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>';
+  }
+  if (element["level-label"] == "skillfull") {
+    level =
+      '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>';
+  }
+  if (element["level-label"] == "experienced") {
+    level =
+      '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>';
+  }
+  if (element["level-label"] == "Professional") {
+    level =
+      '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>';
+  }
+  document.querySelector(".skills-main-division").insertAdjacentHTML(
+    "beforeBegin",
+    `  <div class="skills-division">
+      <p class="skill-name">${element["skill-name"]}  <span class="level">Level: ${element["level-label"]}   </span><span class="stars>${level}</span></p>
+      
+    </div>
+    <hr class="hr"/>`
+  );
+});
+
+references.forEach((element) => {
+  document.querySelector(".references-main-division").insertAdjacentHTML(
+    "beforeBegin",
+    `  <div class="reference-division">
+      <h4 class="referant-name">referant's Name:<span class="refenerence-name-span">${element["reference-name"]}</span></h4>
+      <p class="referant-contact">Contact Information:<span  class="refenerence-contact-span">${element["reference-email"]}</span></p>
+     
+    </div>
+    <hr class="hr"/>`
+  );
+});
